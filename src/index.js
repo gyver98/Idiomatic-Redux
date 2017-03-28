5dcc07ec1278b55ce577361903ca5de5f4db711f
somdari <matthewchoi7@gmail.com>
2017-03-28 22:10:23 +1100

    adding lodash/throttle

--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ import { createStore } from 'redux';
 import todoApp from './reducers';
 import App from './components/App';
 import { loadState, saveState } from './localStorage';
+import throttle from 'lodash/throttle';
 
 const persistedState = loadState();
 const store = createStore(todoApp, persistedState);
 
-store.subscribe(() => {
+store.subscribe(throttle(() => {
   saveState({
     todos: store.getState().todos
   });
-})
+}, 1000));
 
 render(
   <Provider store={store}>
