0428e1728df58b4c3fc4b8faf8c433ae8787f5c0
somdari <matthewchoi7@gmail.com>
2017-03-29 19:51:04 +1100

    adding Root.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './components/App';
 import configureStore from './configureStore';
+import Root from './components/Root';
 
 const store = configureStore();
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Root store={store} />,
   document.getElementById('root')
 );
