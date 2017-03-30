acd5617d7cfb90b4937bdeb4277ef0200c1d127a
somdari <matthewchoi7@gmail.com>
2017-03-30 19:36:17 +1100

    downgrade react-router

--- src/components/Root.js
+++ src/components/Root.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
 
+
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router history={browserHistory}>
