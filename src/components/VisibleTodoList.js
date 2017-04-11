8f1eeb402ca1f5d349d70522f0a7f4a8d089b222
somdari <matthewchoi7@gmail.com>
2017-04-11 21:51:22 +1000

    update VisibleTodoList

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -3,28 +3,9 @@ import { withRouter } from 'react-router';
 import { toggleTodo } from '../actions';
 import TodoList from './TodoList';
 
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case 'all':
-      return todos;
-    case 'completed':
-      return todos.filter(t => t.completed);
-    case 'active':
-      return todos.filter(t => !t.completed);
-    default:
-      throw new Error(`Unknown filter: ${filter}.`);
-  }
-};
-
 const mapStateToProps = (state, { params }) => ({
   todos: getVisibleTodos(state.todos, params.filter || 'all'),
 });
-
-// const mapDispatchToProps = (dispatch) => ({
-//   onTodoClick(id) {
-//     dispatch(toggleTodo(id));
-//   },
-// });
 
 const VisibleTodoList = withRouter(connect(
   mapStateToProps,
