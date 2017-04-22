354428c702c03727ac385fd0160a51c0e40775cb
somdari <matthewchoi7@gmail.com>
2017-04-22 18:48:49 +1000

    adding receiveTodos action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
 import { v4 } from 'node-uuid';
 
+export const receiveTodos = (response) => ({
+    type: 'RECEIVE_TODOS',
+    respose,
+});
+
 export const addTodo = (text) => ({
     type: 'ADD_TODO',
     id: v4(),
