72f3050e924a8e419527bb91d86f7b231562e51b
somdari <matthewchoi7@gmail.com>
2017-04-25 20:45:55 +1000

    update

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import * as actions from '../actions';
@@ -19,27 +19,34 @@ class VisibleTodoList extends Component {
 
   fetchData() {
     const { filter, receiveTodos } = this.props;
-    fetchTodos(filter).then(todos => 
+    fetchTodos(filter).then(todos =>
       receiveTodos(filter, todos)
-    );  
+    );
   }
+
   render() {
     const { toggleTodo, ...rest } = this.props;
     return (
-      <TodoList 
-        {...rest} 
+      <TodoList
+        {...rest}
         onTodoClick={toggleTodo}
       />
-      );
+    );
   }
 }
+
+VisibleTodoList.propTypes = {
+  filter: PropTypes.oneOf(['all', 'active', 'completed']).isRequired,
+  receiveTodos: PropTypes.func.isRequired,
+  toggleTodo: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = (state, { params }) => {
   const filter = params.filter || 'all';
   return {
     todos: getVisibleTodos(state, filter),
-    filter
-  }
+    filter,
+  };
 };
 
 VisibleTodoList = withRouter(connect(
