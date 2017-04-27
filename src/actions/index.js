2e2fa334c4dae057f9114857bc65e6152ce39590
somdari <matthewchoi7@gmail.com>
2017-04-27 20:07:58 +1000

    add api.fetchTodos

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import { v4 } from 'node-uuid';
 import * as api from '../api';
 
-export const receiveTodos = (filter, response) => ({
+const receiveTodos = (filter, response) => ({
   type: 'RECEIVE_TODOS',
   filter,
   response,
